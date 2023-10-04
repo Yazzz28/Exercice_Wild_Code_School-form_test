43a599a1fe647a41adc75798d14779931223b22f
Yacine MENNAA <124800993+Yazzz28@users.noreply.github.com>
2023-10-04 08:37:50 +0200

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -1,7 +1,11 @@
 <?php
 
-echo 'Merci ' . $_POST['user_name'] . ' ' . $_POST['user_firstName']
+
+
+if ($_POST){
+echo 'Merci ' . $data['user_name'] . ' ' . $data['user_firstName']
  . ' de nous avoir contacté à propos de ' 
-. $_POST['sujet'] . '. <br/> Un de nos conseillers vous contactera soit
- à l\'adresse : ' . $_POST['user_email'] . ' ou par téléphone au ' . $_POST['user_phone'] . 
- ' dans les plus brefs délais pour traiter votre demande : <br/>' . $_POST['user_message'];
+. $data['raison'] . '. <br/> Un de nos conseillers vous contactera soit
+ à l\'adresse : ' . $data['user_email'] . ' ou par téléphone au ' . $data['user_phone'] . 
+ ' dans les plus brefs délais pour traiter votre demande : <br/>' . $data['user_message'];
+};
